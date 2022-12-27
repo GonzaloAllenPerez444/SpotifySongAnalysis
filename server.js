86f8d1bdc50ab0958f83eb99b7077c78e8067d24
Gonzalo <gonzaloallenperez444@gmail.com>
2022-12-27 14:10:50 -0800

    more express shenanigans

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -10,3 +10,33 @@ console.log("in server.js");
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname + '/public/index.html'))
 })
+
+app.get("/api",
+
+
+
+
+(req,res) => {
+
+
+    //res.send("BEBEBEEBEBBEE")
+    
+    const spawn = require('child_process').spawn;
+    const process = spawn('python',['./main.py',
+    req.query.URI]);
+
+    process.stdout.on('data', (data) => {res.send(data.toString())});
+
+    //res.json({"myKey": ["this will be ","spotify","stats from the api..."]})
+}
+
+)
+
+
+app.get("/bruh",
+
+
+(req,res) => {res.send('how u doin')}
+
+
+)
