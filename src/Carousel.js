95020716ffffbc735e47644b8e1e0f5214e4817d
Gonzalo <gonzaloallenperez444@gmail.com>
2023-01-06 15:01:51 -0800

    slide updates

--- src/Carousel.js
+++ src/Carousel.js
@@ -27,6 +27,7 @@ const Carousel = (props) =>{
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
@@ -39,29 +40,55 @@ const Carousel = (props) =>{
       
       <div className="distance-split"> </div>
       <h4 className="statTitle"> Popularity: {props.popularity}/100</h4>
-      <div className="distance-bottom"></div>
+      <div className="distance-split"></div>
       <div className="carousel-caption d-none d-md-block">
     
       </div>
     </div>
     <div className="carousel-item">
     <div className="distance-top"> </div>
-      <h1 className="statTitle">ARTIST NAME HERE: </h1>
+      <h1 className="statTitle">Danceability: {props.dance}  </h1>
       <div className="distance-bottom"> </div>
       <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        
+        <p>Danceability is how suitable a song is for dancing to. How can we
+          measure this? It's actually based on a combination of musical elements, ranging from tempo,
+           rhythm stability, beat strength, and overall regularity.</p>
       </div>
     </div>
     <div className="carousel-item">
     <div className="distance-top"> </div>
-      <h1 className="statTitle">POPULARIY SCORE HERE</h1>
+      <h1 className="statTitle">Energy: {props.energy}</h1>
       <div className="distance-bottom"> </div>
       <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        
+        <p>The energy of a song can be influenced by loudness and instrumentation, 
+          but energy can also change when the lyrics reflect more emotion rather than 
+          observation, through different chords, and can rise with a song's melody. 
+        </p>
       </div>
     </div>
+
+
+    <div className="carousel-item">
+    <div className="distance-top"> </div>
+      <h1 className="statTitle">Energy2- make this an actual valid slide: {props.energy}</h1>
+      <div className="distance-bottom"> </div>
+      <div className="carousel-caption d-none d-md-block">
+        
+        <p>The energy of a song can be influenced by loudness and instrumentation, 
+          but energy can also change when the lyrics reflect more emotion rather than 
+          observation, through different chords, and can rise with a song's melody. 
+        </p>
+      </div>
+    </div>
+
+
+
+
+
+
+
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
